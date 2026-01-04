edd570329e8e15f1a426dbe7fdf028d795724236
Sanara-Perera <sanaraperera2003@gmail.com>
2026-01-04 19:43:21 +0530

    Added comments to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+/**
+ * index.js
+ * Application entry point - mounts the React app to the DOM
+ * Wraps the app in StrictMode for development warnings and best practices
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Get the root DOM element where React will mount
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
